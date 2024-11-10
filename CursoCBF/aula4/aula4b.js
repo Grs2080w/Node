431a2e6c8dd755fd70a2ef7722219e9f9e8cbf5d
Grs2080w <grs_santos2019@outlook.com>
2024-11-10 00:22:36 -0300

    testes

--- CursoCBF/aula4/aula4b.js
+++ CursoCBF/aula4/aula4b.js
@@ -3,6 +3,16 @@
 const express = require('express')
 const app = express()
 const porta = process.env.PORT // Caso não souber qual porta está rodando o servidor
+const fs = require('fs')
+
+
+app.get('/teste', (req, res)=>{
+    fs.readFile('erro.html', (err, arq)=>{
+        res.set({'Content-Type':'*/*'}) // Seta o header
+        res.status(404).send('oi') // Envia uma mensagem de erro personalizada
+    })
+    
+})
 
 app.get('/', (req, res)=>{
     res.send('Seja Bem vindo')
@@ -17,4 +27,4 @@ app.get('/canal', (req, res)=>{
     })
 })
 
-app.listen(porta || 3000, ()=>{console.log('Server Connected')}) 
+app.listen(3000, ()=>{console.log('Server Connected')}) 
